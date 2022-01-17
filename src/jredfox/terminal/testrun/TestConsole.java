b62abf58af1826dfdce1576fe802e60e57b6e041
jredfox <jredfox@hotmail.com>
2022-01-17 12:11:59 -0600

    Update TestConsole.java
    backup

--- src/jredfox/terminal/testrun/TestConsole.java
+++ src/jredfox/terminal/testrun/TestConsole.java
@@ -3,6 +3,7 @@ package jredfox.terminal.testrun;
 import java.io.IOException;
 import java.util.UUID;
 
+import jredfox.common.utils.JavaUtil;
 import jredfox.terminal.OpenTerminal;
 import jredfox.terminal.app.ITerminalApp;
 import jredfox.terminal.app.TerminalApp;
@@ -11,7 +12,8 @@ public class TestConsole implements ITerminalApp {
 	
 	public static void main(String[] args) throws IOException
 	{
-		TerminalApp app = OpenTerminal.INSTANCE.run(TestConsole.class, args);
+		System.out.println("\u2728\u02dc\u201d*\u00b0\u2022\ud835\ude83\ud835\ude8a\ud835\ude9c\ud835\ude9d\ud835\udea2_\ud835\ude71\ud835\ude92\ud835\ude9c\ud835\ude8c\ud835\ude9e\ud835\ude92\ud835\ude9d\u2022\u00b0*\u201d\u02dc\u2728#6161");
+//		TerminalApp app = OpenTerminal.INSTANCE.run(TestConsole.class, args);
 	}
 
 	@Override
