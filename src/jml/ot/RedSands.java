0165eb95b334da7c57ae1854249ff0bd725a34ea
jredfox <jredfox_@outlook.com>
2022-06-02 10:08:14 -0500

    improve closeMe.scpt
    improve logic of closeMe applescript

---
+++ src/jml/ot/RedSands.java
@@ -0,0 +1,22 @@
+package jml.ot;
+
+public class RedSands {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("starting redsands...");
+		while(true)
+		{
+			try 
+			{
+				Thread.sleep(1000);
+			} 
+			catch (InterruptedException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
