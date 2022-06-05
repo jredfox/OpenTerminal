c77de0e7652440a993800c86ca7df36799180412
jredfox <jredfox_@outlook.com>
2022-06-05 17:22:24 -0500

    remove dupe terminals
    organize terminals by name and remove duplicate terminals

--- src/jml/ot/RedSands.java
+++ src/jml/ot/RedSands.java
@@ -1,32 +1,44 @@
 package jml.ot;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class RedSands {
 	
 	public static void main(String[] args) throws IOException
 	{
-//		new ProcessBuilder(new String[] 
+		Set<String> list = new TreeSet<>(new Comparator<String>()
+				{
+					@Override
+					public int compare(String t1, String t2) 
+					{
+						return t1.toLowerCase().compareTo(t2.toLowerCase());
+					}
+			
+				}
+			);
+		for(String s : OSUtil.linux_terminals)
+			list.add(s);
+		for(String s : list)
+			System.out.println("\"" + s + "\",");
+//		System.out.println("starting redsands...");
+//		while(true)
 //		{
-//			"open",
-//			"-gj",
-//			"-F",
-//			"-a",
-//			"Terminal"
-//		}).start();
-		System.out.println("starting redsands...");
-		while(true)
-		{
-			try 
-			{
-				Thread.sleep(1000);
-			} 
-			catch (InterruptedException e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+//			try 
+//			{
+//				Thread.sleep(1000);
+//			} 
+//			catch (InterruptedException e) 
+//			{
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
 	}
 
 }
