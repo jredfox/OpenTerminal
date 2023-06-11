b3292fb31f4be891ee9cec9885f6e33008182643
jredfox <jredfox_@outlook.com>
2023-06-10 21:18:18 -0500

    win isProcessAlive

---
+++ src-c++/pidia-win.cpp
@@ -0,0 +1,63 @@
+//============================================================================
+// Name        : pidia.cpp
+// Author      : jredfox
+// Version     : beta 1.0.0
+// Copyright   : Your copyright notice
+// Description : PID is alive WINDOWS branch
+//============================================================================
+
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+bool isProcessAlive(DWORD pid);
+string toString(bool b);
+void testIsAlive();
+
+map<DWORD, string> m;
+int main()
+{
+
+}
+
+void testIsAlive()
+{
+	DWORD pid;
+	while(true)
+	{
+		cout << "Enter PID:";
+		cin >> pid;
+		cout << "PID " << pid << " isAlive:" << toString(isProcessAlive(pid)) + "\n";
+	}
+}
+
+map<DWORD, HANDLE> handles;
+bool isProcessAlive(DWORD pid)
+{
+	HANDLE process;
+	if(handles.find(pid) == handles.end())
+	{
+		process = OpenProcess(SYNCHRONIZE, FALSE, pid);
+		handles[pid] = process;
+	}
+	else
+	{
+		process = handles[pid];
+	}
+	DWORD ret = WaitForSingleObject(process, 0);
+	bool isRunning = ret == WAIT_TIMEOUT;
+	if(!isRunning)//close the cached handle to free the PID and erase from the cache
+	{
+		CloseHandle(process);
+		handles.erase(pid);
+	}
+    return isRunning;
+}
+
+string toString(bool b)
+{
+	return b ? "true" : "false";
+}
--- src/jml/ot/Debug.java
+++ src/jml/ot/Debug.java
@@ -1,20 +1,13 @@
 package jml.ot;
 
+import java.awt.Color;
 import java.io.IOException;
 
 public class Debug {
 
 	public static void main(String[] args) throws InterruptedException, IOException
 	{
-		Runtime.getRuntime().addShutdownHook(new Thread() 
-		{
-			@Override
-			public void run()
-			{
-				System.out.println("shutting down now");
-			}
-		});
-		System.out.println("end the program");
+		
 	}
 	
 }
