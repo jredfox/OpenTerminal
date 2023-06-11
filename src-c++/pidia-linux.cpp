f436a80c20e35517c556e8938b03d4b19fd69ca3
jredfox <jredfox_@outlook.com>
2023-06-11 17:25:55 -0500

    Update pidia-linux.cpp

--- src-c++/pidia-linux.cpp
+++ src-c++/pidia-linux.cpp
@@ -19,50 +19,6 @@ void testIsAlive();
 string toString(bool b);
 bool isPIDAlive(unsigned long pid);
 
-int getProcIdByName(string procName)
-{
-    int pid = -1;
-
-    // Open the /proc directory
-    DIR *dp = opendir("/proc");
-    if (dp != NULL)
-    {
-        // Enumerate all entries in directory until process found
-        struct dirent *dirp;
-        while (pid < 0 && (dirp = readdir(dp)))
-        {
-            // Skip non-numeric entries
-            int id = atoi(dirp->d_name);
-            if (id > 0)
-            {
-                // Read contents of virtual /proc/{pid}/cmdline file
-                string cmdPath = string("/proc/") + dirp->d_name + "/cmdline";
-                ifstream cmdFile(cmdPath.c_str());
-                string cmdLine;
-                getline(cmdFile, cmdLine);
-                if (!cmdLine.empty())
-                {
-                    // Keep first cmdline item which contains the program path
-                    size_t pos = cmdLine.find('\0');
-                    if (pos != string::npos)
-                        cmdLine = cmdLine.substr(0, pos);
-                    // Keep program name only, removing the path
-                    pos = cmdLine.rfind('/');
-                    if (pos != string::npos)
-                        cmdLine = cmdLine.substr(pos + 1);
-                    // Compare against requested process name
-                    if (procName == cmdLine)
-                        pid = id;
-                }
-            }
-        }
-    }
-
-    closedir(dp);
-
-    return pid;
-}
-
 std::map<unsigned long, DIR*> handles = {};
 /**
  * returns true if the process is alive and attempts to suggest a handle to linux's os that we are reading the directory /proc/[PID] reserve this PID till program shutdown
