252bdf0ed651071b50f4faff5465391e8bc35bed
jredfox <jredfox_@outlook.com>
2023-06-13 00:13:54 -0500

    WIP
    wip new API to match issue 48

--- src-c++/pidia-win.cpp
+++ src-c++/pidia-win.cpp
@@ -6,10 +6,16 @@
 // Description : PID is alive WINDOWS branch
 //============================================================================
 
-#include <windows.h>
+#include <process.h>
+#include <winbase.h>
+#include <windef.h>
+#include <winerror.h>
+#include <winnt.h>
 #include <iostream>
-#include <vector>
 #include <map>
+#include <string>
+#include <signal.h>
+#include <sys/types.h>
 
 using namespace std;
 
@@ -34,6 +40,58 @@ void testIsAlive()
 			break;
 		cout << "PID " << pid << " isAlive:" << toString(isProcessAlive(pid)) + "\n";
 	}
+}
+
+/**
+ * returns the current process's id
+ */
+unsigned long getPID()
+{
+	return getpid();
+}
+
+unsigned long getProcessTime(unsigned long pid)
+{
+	return -1;//TODO:
+}
+
+unsigned long getProcessTime()
+{
+	return getProcessTime(getPID());
+}
+
+long unsigned getPID(string process_name)
+{
+	return -1;//TODO:
+}
+
+void sendSignal(unsigned long pid, int signal)
+{
+	//TODO:
+}
+
+/**
+ * equal to System#exit(0) hopefully
+ */
+void stopProcess(unsigned long pid)
+{
+	sendSignal(pid, SIGINT);//TODO figure out if SIGSTOP should be here instead
+}
+
+/**
+ * abnormal termination of a process. I believe there is a 2 second delay before the OS closes the process or sends a SIGKILL
+ */
+void terminateProcess(unsigned long pid)
+{
+	sendSignal(pid, SIGTERM);
+}
+
+/**
+ * kills the process without being handled by System#exit. this may cause file corruption
+ */
+void killProcesss(unsigned long pid)
+{
+	sendSignal(pid, 9);//SIGKILL is 9 on all CPUS ARCS and isn't defined on windows
 }
 
 map<DWORD, HANDLE> handles;
